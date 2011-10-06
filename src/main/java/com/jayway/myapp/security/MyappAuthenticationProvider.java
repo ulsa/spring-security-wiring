845d52c048af2301df0e970c735a5f59dd6e7fa5
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2011-10-06 10:05:51 +0200

    Now using component scanning and autowire and no manually wired beans.

--- src/main/java/com/jayway/myapp/security/MyappAuthenticationProvider.java
+++ src/main/java/com/jayway/myapp/security/MyappAuthenticationProvider.java
@@ -7,7 +7,9 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
 
+@Component("authenticationProvider")
 public class MyappAuthenticationProvider implements AuthenticationProvider {
 
     @Autowired
