8dc573a61eb7edee57a71d2aa5bcd2b94886de36
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2011-10-06 10:55:21 +0200

    Added better printouts to see what's happening.

--- src/main/java/com/jayway/myapp/security/MyappAuthenticationProvider.java
+++ src/main/java/com/jayway/myapp/security/MyappAuthenticationProvider.java
@@ -1,7 +1,10 @@
 package com.jayway.myapp.security;
 
 import com.jayway.myapp.service.MyService;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,11 +12,22 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 @Component("authenticationProvider")
-public class MyappAuthenticationProvider implements AuthenticationProvider {
+public class MyappAuthenticationProvider implements AuthenticationProvider, ApplicationContextAware {
 
     @Autowired
     private MyService service;
+
+    public MyappAuthenticationProvider() {
+        System.out.println("=====> Creating " + this);
+        StringWriter out = new StringWriter();
+        new Throwable().printStackTrace(new PrintWriter(out));
+        System.out.println("=====> No exception has been thrown, just printing the current stack trace: "
+                + out.toString());
+    }
 
     @Override
     public Authentication authenticate(Authentication authenticationRequest) throws AuthenticationException {
@@ -26,5 +40,10 @@ public class MyappAuthenticationProvider implements AuthenticationProvider {
     @Override
     public boolean supports(Class<?> authentication) {
         return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("=====> " + this + ": My applicationContext is: " + applicationContext);
     }
 }
--- src/main/java/com/jayway/myapp/service/MyServiceImpl.java
+++ src/main/java/com/jayway/myapp/service/MyServiceImpl.java
@@ -1,13 +1,25 @@
 package com.jayway.myapp.service;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyServiceImpl implements MyService {
+public class MyServiceImpl implements MyService, ApplicationContextAware {
+    public MyServiceImpl() {
+        System.out.println("=====> Creating " + this);
+    }
+
     @Override
     public Authentication doSomething(Authentication authentication) {
         System.out.println("in MyServiceImpl#doSomething()");
         return authentication;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("=====> " + this + ": My applicationContext is: " + applicationContext);
     }
 }
